b2e9561e1ab02ba0dc2a63a883f8b33d8495096e
vibueno <20514260+vibueno@users.noreply.github.com>
2020-12-14 21:46:32 +0100

    bug leading zeros in operands

--- js/main.js
+++ js/main.js
@@ -19,16 +19,24 @@ const calculatorClickHandler = e => {
     if (button.classList.contains(BTN_CLEAR_CLASS)) {
       functions.clearScreen();
       calculator.clearHistory();
-    } //Button equals
+    }
+    //Button equals
     else if (button.classList.contains(BTN_EQUALS_CLASS)) {
-      const expression = SCREEN.innerText.replace('x', '*');
+      let expression = SCREEN.innerText.replace('x', '*');
+
+      //deletes leading 0's of left operand of 1st operation on screen
+      expression = expression.replace(/^0+([^.])/g, '$1');
+      //deletes leading 0's of operand after operator
+      expression = expression.replace(/([\+\-\*\/])0+([^.])/g, '$1$2');
+
       const result = calculator.calculateResult(expression).toString();
       if (typeof result != 'undefined') {
         calculator.addToHistory(SCREEN.innerText);
         calculator.addToHistory(result);
         functions.clearScreen();
         functions.printScreen(result);
-      } //All other buttons
+      }
+      //All other buttons
     } else functions.printScreen(value);
   }
 };
